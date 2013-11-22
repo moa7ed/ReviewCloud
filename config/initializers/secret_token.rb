4ad1080398a217988a87b44794383e55870f6e26
moa7ed <moa7ed25@gmail.com>
2013-11-22 12:05:01 +0200

    create ReviewCloud

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reviewcloud::Application.config.secret_token = '530b68c68fc6229a7a4f410a524fb61f40491eb9867653d1d3596473b7f5b8d8aca1270310aa09431a9233fe37b00d34beca5d8f773704fb5910f883e48a3280'
